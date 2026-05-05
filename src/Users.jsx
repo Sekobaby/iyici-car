562338d3d4ea93911553dbc48c9332f0e46b40c2
Burak Kaan Gök <burak_kaan123@hotmail.com>
2026-05-05 22:09:02 +0300

    admin ekleme güncellendi

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { supabase, supabaseAdmin } from "./supabaseClient";
-import { supabase } from "./supabaseClient";
+//import { supabase } from "./supabaseClient";
 import {
   Trash2,
   UserPlus,
